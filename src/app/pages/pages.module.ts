cefd4ea996628d1314bb43d2a449f67fc89b931d
Diogo Eleutero <desdiogo10@yahoo.com>
2021-08-16 10:17:47 -0300

    Add PageModule

---
+++ src/app/pages/pages.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+//Module
+import { RoutingModule } from './routing.module';
+
+//Pages
+import { HomeComponent } from './home/home.component';
+import { DetailsComponent } from './details/details.component';
+
+
+
+
+@NgModule({
+  declarations: [
+    HomeComponent,
+    DetailsComponent
+  ],
+  imports: [
+    CommonModule,
+    RoutingModule
+  ]
+})
+export class PagesModule { }
